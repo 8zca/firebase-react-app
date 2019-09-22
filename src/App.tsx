84edcd75e5a559a30e856c33e7bd0721cfb1424e
8zca <kazuya.m@sora.flights>
2019-09-22 16:25:47 +0900

    intial app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { ProvideAuth } from './auth'
+import Index from './components/index'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ProvideAuth>
+      <Router>
+        <Route path='/' exact component={Index} />
+      </Router>
+    </ProvideAuth>
+  )
 }
 
-export default App;
+export default App
