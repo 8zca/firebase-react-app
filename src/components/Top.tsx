139742386b3c6251e0dd7ac255ca14808e27eb0d
8zca <excref@gmail.com>
2020-08-03 11:12:58 +0900

    [Add] ページ遷移サンプル追加

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,32 +1,31 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { RouteComponentProps, Link } from 'react-router-dom'
 import firebase from 'services/firebase'
 
-const Top = () => {
+const Top = (props: RouteComponentProps) => {
+  const [token, setToken] = useState<string>('')
 
   useEffect(() => {
     const currentUser = firebase.auth().currentUser
     if (currentUser) {
       currentUser.getIdToken(true).then(token => {
-        axios.get(process.env.REACT_APP_API_URL as string, {
-          headers: {
-            'X-Firebase-Token': token,
-            'Content-Type': 'application/json;charset=utf-8'
-          },
-          data: {}
-        })
-        .catch(res => {
-          console.log(res)
-        })
-  
+        setToken(token)
       }).catch(err => console.log(err))
     }
   }, [])
 
+  const logout = () => {
+    firebase.auth().signOut()
+    props.history.push('/login')
+  }
 
   return (
-    <div>
+    <div style={{ padding: '40px' }}>
       <h1>Welcom to Top page!</h1>
+      <pre style={{ width: '400px', whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}>{token}</pre>
+      <Link to='/currentUser'>currentUser</Link>
+      <br />
+      <button onClick={logout}>ログアウト</button>
     </div>
   )
 }
